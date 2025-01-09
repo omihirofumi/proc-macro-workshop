2da659d4fb9a154805c50067d4637f887d4dac96
Hirofumi Omi <hirofumi.omi@3-shake.com>
2025-01-09 11:18:32 +0900

    builder:test2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,32 @@
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = syn::parse(input).unwrap();
 
-    unimplemented!()
+    impl_derive_macro(&ast)
+}
+
+fn impl_derive_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+    pub struct CommandBuilder {
+        executable: Option<String>,
+        args: Option<Vec<String>>,
+        env: Option<Vec<String>>,
+        current_dir: Option<String>,
+    }
+            impl #name {
+                pub fn builder() -> CommandBuilder {
+                    CommandBuilder {
+                        executable: None,
+                        args: None,
+                        env: None,
+                        current_dir: None
+                    }
+                }
+            }
+        };
+    gen.into()
 }
